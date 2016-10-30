27b657dfc26ff0ce6655ea4047ba9a40d48ab45f
Bivu Raj Dhungana <brdhunga@gmail.com>
2016-10-30 17:55:19 -0500

    filterEventsById--function to filter events by id--test passed

--- src/reducers/event.test.js
+++ src/reducers/event.test.js
@@ -4,6 +4,7 @@ import App from '../App';
 import expect from 'expect';
 
 import activeCategoryReducer from './activeCategoryReducer'
+import { filterEventsById, eventReducer } from './eventReducer'
 
 
 function name(name_){
@@ -14,5 +15,32 @@ it('activeCategory reducer works', () => {
     const action_ = {type: 'CHANGE_ACTIVE_CATEGORY', category: "university"}
     expect(activeCategoryReducer({}, action_)).toEqual("university")
 });
+
+
+const events = [
+    {name: "event 1", id: "event1", reminderAdded:false },
+    {name: "another event", id:"event2", reminderAdded:false}
+]
+
+// check for event 1
+const event1 = events[0]
+const id1 = "event1"
+const id2 = "event2"
+const nonExistId = "event3"
+
+//
+it ("event filterer returns unique event", () => {
+    expect(filterEventsById(events, id1)).toEqual(event1)
+})
+
+//
+it ("event filterer does not return different event", () => {
+    expect(filterEventsById(events, id2)).toNotEqual(event1)
+})
+
+//
+it ("event filterer returns null for  non existant event", () => {
+    expect(filterEventsById(events, nonExistId)).toEqual(null)
+})
 
 
---
+++ src/reducers/eventReducer.js
@@ -0,0 +1,25 @@
+export function filterEventsById(events_, id_){
+    /*
+     filters events to get particular event by id
+     */
+    for (var index in events_){
+        if (events_[index].id === id_){
+            return events_[index]
+        }
+    }
+    return null;
+}
+
+
+export function eventReducer(state = [], action) {
+    /* the input is list of events */
+    switch(action.type) {
+        case 'REMINDER_ADDED':
+            const chosenEvent = filterEventsById(state, action.id)
+            const newEvent = {...chosenEvent, reminderAdded: true}
+            const index = state.indexOf(chosenEvent)
+            return [...state.slice(0, index), newEvent ,...state.slice(index+1)]
+    }
+
+    return state;
+}
